a09888c876afdf0be8b8c4259af6acca13a15774
faezareyhn-spec <faezareyhn@gmail.com>
2026-07-13 07:27:19 +0800

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,13 @@
+import dotenv from "dotenv";
+import app from "./app";
 
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("====================================");
+    console.log("🚀 Telegram Mail Platform Started");
+    console.log(`🌐 Server : http://localhost:${PORT}`);
+    console.log("====================================");
+});
