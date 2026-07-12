1f567cb9b4b32d4bece951f84e99703f7e298229
faezareyhn-spec <faezareyhn@gmail.com>
2026-07-13 07:26:56 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
+import express from "express";
+import helmet from "helmet";
 
+const app = express();
+
+app.use(helmet());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({
+        status: "online",
+        message: "Telegram Mail Platform API"
+    });
+});
+
+export default app;
